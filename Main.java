36d94c557f9dceb3605602b507e3b068c945f52c
Shagun Singh <shagundun@gmail.com>
2025-07-05 08:59:40 +0530

    Fixed data type in file size, added Main class for testing parameterized constructor

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        digitalLibraryItem newBook = new digitalLibraryItem("Little Women", "Louisa May Alcott", "Book", 4.7);
+        newBook.displayInfo();
+    }
+}
--- digitalLibraryItem.java
+++ digitalLibraryItem.java
@@ -3,7 +3,7 @@ public class digitalLibraryItem
     public String title;
     public String creator;
     public String type;
-    public int fileSize;
+    public double fileSize;
     digitalLibraryItem() // default constructor, default initialization takes place here when no arguments given
     {
         title = "";
@@ -12,7 +12,7 @@ public class digitalLibraryItem
         fileSize = -1;
     }
     
-    digitalLibraryItem(String title, String creator, String type, int fileSize)
+    digitalLibraryItem(String title, String creator, String type, double fileSize)
     {
         this.title = title;
         this.creator = creator;
@@ -24,7 +24,7 @@ public class digitalLibraryItem
         System.out.println("Title: "+title);
         System.out.println("Creator: "+creator);
         System.out.println("Type of media: "+type);
-        System.out.println("File size: "+fileSize);
+        System.out.println("File size: "+fileSize +" MB");
         
     }
 }
