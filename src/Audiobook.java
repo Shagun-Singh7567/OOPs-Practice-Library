cbcd95bc38d1735b4fc3cc76ca2cc40bddb738b4
Shagun Singh <shagundun@gmail.com>
2025-07-11 17:55:13 +0530

    feat: Added subclass Audiobook

---
+++ src/Audiobook.java
@@ -0,0 +1,18 @@
+public class Audiobook extends DigitalLibraryItem{
+    private String narrator;
+    private double durationInHours;
+    Audiobook(String title, String creator, String type, double fileSize, String narrator, double durationInHours)
+    {
+        super(title, creator, type, fileSize);
+        this.narrator = narrator;
+        this.durationInHours = durationInHours;
+
+    }
+    public void displayInfo()
+    {
+        super.displayInfo();
+        System.out.println("Narrator: "+narrator);
+        System.out.println("Duration in hours: "+durationInHours);
+
+    }
+}
