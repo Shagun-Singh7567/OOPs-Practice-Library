e0d555a63373ff9c88ec98758c3354b0517a649d
Shagun Singh <shagundun@gmail.com>
2025-07-15 08:36:33 +0530

    Implemented interface Readable and implemented to Book class

--- src/items/Book.java
+++ src/items/Book.java
@@ -1,5 +1,5 @@
 package items;
-public class Book extends DigitalLibraryItem  {
+public class Book extends DigitalLibraryItem implements Readable {
 
     private int numberOfPages;
     Book(String title, String creator, String type, double fileSize, int numberOfPages)
@@ -24,5 +24,11 @@ public class Book extends DigitalLibraryItem  {
     {
         super.displayInfo();
         System.out.println("Number of pages: "+ numberOfPages);
+    }
+
+    public void flipPage()
+    {
+        for(int i = 1; i <= numberOfPages; i++)
+        System.out.println("Page number "+ i);
     }
 }
